SeedlingsBabylab/audiowords

commit d2fc27ef98bca3838d328812ae29dd7c58b25e32

Fix #3
There was a bug where a sound with equal onset/offset would make it
into the silence list and eventually into the CLAN file. Added an extra
check to skip these kinds of entries.

silences.py
@@ -21,6 +21,8 @@ class SilenceParser:
                 # into an array of floats called sound
                 sound = [float(entries[0])*1000, float(entries[1])*1000]
 
+                if sound[0] == sound[1]:
+                    continue
                 # if the length of the sound is greater than the
                 # minimum sound length, or entry is an [End] marker,
                 # add it to the sounds[], which will be returned
@@ -28,9 +30,7 @@ class SilenceParser:
                 if (sound[1] - sound[0]) > self.min_sound_length\
                         or entries[2] == "[End]":
                     sounds.append(sound)
-
         return sounds
-
 
     def parse_silences(self, sounds):
 
@@ -71,7 +71,6 @@ class SilenceParser:
                 num += 1
 
         return silences
-
 
 class Silence(object):
 

clanfile.py
@@ -10,9 +10,6 @@ class ClanFileParser:
         self.silences_inserted = False
         self.overlaps_inserted = False
 
-
-
-
     def insert_silences(self, silences):
 
         # we initialize a queue of silences using the
@@ -22,7 +19,6 @@ class ClanFileParser:
 
         # open the export clan file
         output = open(self.export_clan_file, "w")
-
 
         with open(self.clan_file, "rU") as file:
 
@@ -46,7 +42,6 @@ class ClanFileParser:
             for index, raw_line in enumerate(file):
                 # get rid of preceding and trailing whitespace from the line
                 line = raw_line.strip()
-
 
                 # We only write comments after lines with " *XYZ: " prefixes.
                 # The check for curr_silence ensures that there is still a
@@ -154,7 +149,6 @@ class ClanFileParser:
 
         output.close()
 
-
     def insert_overlaps(self, region_values, region_map, silences):
         """
 
@@ -171,8 +165,6 @@ class ClanFileParser:
         lowest_region = region_values[len(region_values) - 1]
         offset_list = region_values # this is a list of interval offsets
 
-
-
         # for index, x in enumerate(region_values):
         #     for key, value in region_map.items():
         #         if value == x:
@@ -186,12 +178,9 @@ class ClanFileParser:
         region_queue = deque(sorted_offsets)
         silence_queue = deque(silences)
 
-
-
         # print "region values: " + str(region_values)
         # print "sorted_offsets: " + str(sorted_offsets)
         # print "region queue: " + str(region_queue)
-
 
         with open(self.clan_file, "rU") as file:
 
@@ -231,7 +220,6 @@ class ClanFileParser:
                 # get rid of leading and trailing whitespace from the line
                 line = raw_line.strip()
 
-
                 # We only write comments after lines with " *XYZ: " prefixes.
                 # The check for curr_silence ensures that there is still a
                 # silence waiting to be written
@@ -249,7 +237,6 @@ class ClanFileParser:
                     current_clan_interval[1] = int(interval[1])
 
                     #print "clan[0]: " + str(current_clan_interval[0]) + "clan[1]: " + str(current_clan_interval[1]) + "    curr_region_start: " + str(curr_region_start)
-
 
                     # Handle special case for 0 offset
                     if curr_region_start == 0:
@@ -477,7 +464,6 @@ class ClanFileParser:
         output.close()
         self.find_interval_errors()
 
-
     def find_interval_errors(self):
 
         # regex object to parse out the timestamp
@@ -491,7 +477,6 @@ class ClanFileParser:
             for index, raw_line in enumerate(file):
                 # get rid of preceding and trailing whitespace from the line
                 line = raw_line.strip()
-
 
                 # We only write comments after lines with " *XYZ: " prefixes.
                 # The check for curr_silence ensures that there is still a

overlaps.py
@@ -107,9 +107,7 @@ class Overlaps:
 
                     self.dataset.data.append((meaningful, awc_actual, ctc_actual, cvc_actual))
 
-
                     #self.dataset.hours()
-
 
             #     print
             #     print "timestamp: " + line[10]
@@ -202,7 +200,6 @@ class Overlaps:
             y += 1  # bump
             hour_buffer = self.dataset.data[x:y]  # re-slice
 
-
         # pull out the N'th index of each element in "regions",
         # which is a list of tuples, with list comprehensions.
         # Each tuple has the average value for the hour chunk
@@ -246,8 +243,6 @@ class Overlaps:
         # print
         # print
 
-
-
     def rank_list(self, list, top_n):
         """
         This builds the regions map, resets decimal precision so
@@ -281,7 +276,6 @@ class Overlaps:
         filtered_list = self.filter_overlaps(sorted_list, region_map, top_n)
         return (region_map, filtered_list)
 
-
     def filter_overlaps(self, list, map, top_n):
         """
         This passes through the ranked offset list and uses the region
@@ -297,10 +291,7 @@ class Overlaps:
         # a certain magnitude
         offset_lists = []
 
-
-
         last_interval = None
-
 
         for index, x in enumerate(list):
             # this is the temporary list that will be pushed into the
@@ -326,9 +317,7 @@ class Overlaps:
                     #last_result = results[len(results)-1]
 
         # print "results: " + str(results)
-
         return results
-
 
     def overlapping(self, previous_regions, this_start):
         """

commit ea6645d667ed49b0bc209b06bb13d934d53b26bc

Fix #6
This bug was caused by a workaround introduced in
d2fc27ef98bca3838d328812ae29dd7c58b25e32. I removed this and added a
new check to handle the old issue and take care of this new one as
well.

silences.py
@@ -21,8 +21,8 @@ class SilenceParser:
                 # into an array of floats called sound
                 sound = [float(entries[0])*1000, float(entries[1])*1000]
 
-                if sound[0] == sound[1]:
-                    continue
+                # if sound[0] == sound[1]:
+                #     continue
                 # if the length of the sound is greater than the
                 # minimum sound length, or entry is an [End] marker,
                 # add it to the sounds[], which will be returned
@@ -67,6 +67,11 @@ class SilenceParser:
                 #
                 # The values are converted back to seconds before construction.
                 # Then, add this Silence object to the silences[] we are going to return
+                if curr_sound[0] == curr_sound[1]:
+                    if curr_sound[0] - prev_sound[1] > 0:
+                        silence = Silence(prev_sound[1]/1000, curr_sound[0]/1000, num)
+                    else:
+                        continue
                 silence = Silence(prev_sound[1]/1000, curr_sound[0]/1000, num)
                 silences.append(silence)
 