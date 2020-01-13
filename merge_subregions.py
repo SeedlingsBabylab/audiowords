90762894ab6d6644e0252c284835a3c7b0122ef9
Sarp Uner <sarpim@gmail.com>
2020-01-13 11:05:29 -0500

    Correctly outputting the required subregion line.

--- merge_subregions.py
+++ merge_subregions.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, sys, difflib
 
 # Function to take care of parsing arguments.
 def get_args():
@@ -8,18 +8,27 @@ def get_args():
     parser.add_argument('--output_file', help='the optional output file to which the merged result will be outputted.')
     return parser.parse_args()
 
+# Quick function to check for correct subregion line (subregion starts or ends)
+def check_sub_line(line):
+    return 'subregion' in line and ('starts' in line or 'ends' in line)
 
 
 if __name__ == '__main__':
 
     # Get parsed arguments.
     args = get_args()
-
+    print 'Arguments that the script has received:'
     print vars(args)
 
+    with open(args.sub_file) as f1:
+        sublines = f1.readlines()
 
+    with open(args.input_file) as f2:
+        mergelines = f2.readlines()
 
-
+    for line in sublines:
+        if check_sub_line(line):
+            print line
 
 
 
