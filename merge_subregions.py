7d6b614c65cffe7a523ddc6f51422a9c1535abe3
Sarp Uner <sarpim@gmail.com>
2020-01-13 14:35:12 -0500

    merge_subregions can now handle those files that have 5 subregions without rank. Will make modifications to handle those files that either a) have less than 5 subregions, b) don\'t have subregions at all (such as month 6 and 7 files)

--- merge_subregions.py
+++ merge_subregions.py
@@ -1,13 +1,4 @@
-import argparse, sys, difflib
-
-# Color output for the terminal
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+import argparse, sys, difflib, heapq
 
 # Function to take care of parsing arguments.
 def get_args():
@@ -43,6 +34,30 @@ def get_user_input(m):
             print('That is not an integer, please choose again!')
     return choice
 
+def get_match_and_indices(word, possibilities, n=3, cutoff=0.6):
+    if not n >  0:
+        raise ValueError("n must be > 0: %r" % (n,))
+    if not 0.0 <= cutoff <= 1.0:
+        raise ValueError("cutoff must be in [0.0, 1.0]: %r" % (cutoff,))
+    result = []
+    s = difflib.SequenceMatcher()
+    s.set_seq2(word)
+    for i, x in enumerate(possibilities):
+        s.set_seq1(x)
+        if s.real_quick_ratio() >= cutoff and \
+           s.quick_ratio() >= cutoff and \
+           s.ratio() >= cutoff:
+            result.append((s.ratio(), x, i))
+
+    # Move the best scorers to head of list
+    result = heapq.nlargest(n, result)
+    # Strip scores for the best n matches
+    return [(x, i) for score, x, i in result]
+
+def write_output(lines, filename='temp.txt'):
+    with open(filename, 'w') as outf:
+        outf.writelines(lines)
+
 
 
 if __name__ == '__main__':
@@ -51,27 +66,41 @@ if __name__ == '__main__':
     args = get_args()
     print 'Arguments that the script has received:'
     print vars(args)
+    print
 
     with open(args.sub_file) as f1:
         sublines = f1.readlines()
 
     with open(args.input_file) as f2:
         mergelines = f2.readlines()
+        # we make a copy of the mergelines (the lines from the file that we are replacing the comments with)
+        # to prevent the possibility of any nasty side effects.
+        resultlines = mergelines[:]
 
+    # Iterate through the lines of the newly created ranked subregion file.
     for line in sublines:
         if check_sub_line(line):
-            print line
+            print 'the replacement line:\n{}\n'.format(line)
+            similars = get_match_and_indices(line, mergelines, cutoff=0.8)
 
-            similars = difflib.get_close_matches(line, mergelines)
-            print 'Please select from the option below the subregion comment you wish to replace'
+            # If the similars list is empty, which means that it couldn't find a close match, abort, and manually check!
+            # IMPORTANT: This part will be modified to handle complications for files that don't have 5 subregions:
+            # an insert anyway option will try to find the appropriate place to insert the line!
+            if not similars:
+                sys.exit()
+
+                
+            print 'Please select from the option below the subregion comment you wish to replace with the replacement line above'
             pretty_print(similars)
             choice = get_user_input(len(similars))
-            print choice
-            
+            print
+            replacement_line, index = similars[choice]
 
+            #garble the line so that it won't be matched for anything else the next round!
+            mergelines[index] = '========================================'
 
+            # actually replacing the line with the ranked line.
+            resultlines[index] = line
 
-
-
-
+    write_output(resultlines)
 
